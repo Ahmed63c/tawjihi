8231952950efcf6bbb7295c0a07c59e985fbae69
Ahmed63c <Ahmedhamdy63c@gmail.com>
2020-08-04 19:07:50 +0200

    home Screen

---
+++ lib/Screens/Course/CourseOptionsScreen.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tawjihi/Screens/ComonWidget/Text.dart';
+import 'package:tawjihi/Utils/ColorProperties.dart';
+
+class CourseList extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: ColorProperties.AppColor,
+        title: Text(
+          'الرياضيات',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Stack(children: <Widget>[
+        ListView(
+          children: <Widget>[
+            Card(
+              elevation: 4,
+              child: ListTile(
+                title: MyText('videos',style: TextStyle(fontFamily: "Cairo",fontSize: 18,fontWeight: FontWeight.w600),),
+                leading: Container(height:double.infinity,child: Icon(Icons.video_library,color: Colors.blue,size: 18,)),
+                trailing: Icon(Icons.arrow_forward_ios,color: Colors.black,size: 14,),
+              ),
+            ),
+            Card(
+              elevation: 4,
+              child: ListTile(
+                title: MyText('articles',style: TextStyle(fontFamily: "Cairo",fontSize: 18,fontWeight: FontWeight.w600),),
+                leading: Container(height:double.infinity,child: Icon(Icons.assignment,color: Colors.blue,size: 18,)),
+                trailing: Icon(Icons.arrow_forward_ios,color: Colors.black,size: 14,),
+              ),
+            ),
+            Card(
+              elevation: 4,
+              child: ListTile(
+                title: MyText('school_exams',style: TextStyle(fontFamily: "Cairo",fontSize: 18,fontWeight: FontWeight.w600),),
+                leading: Container(height:double.infinity,child: Icon(Icons.content_paste,color: Colors.blue,size: 18,)),
+                trailing: Icon(Icons.arrow_forward_ios,color: Colors.black,size: 14,),
+              ),
+            ),
+            Card(
+              elevation: 4,
+              child: ListTile(
+                title: MyText('work_papers',style: TextStyle(fontFamily: "Cairo",fontSize: 18,fontWeight: FontWeight.w600),),
+                leading: Container(height:double.infinity,child: Icon(Icons.assignment_ind,color: Colors.blue,size: 18,)),
+                trailing: Icon(Icons.arrow_forward_ios,color: Colors.black,size: 14,),
+              ),
+            ),
+            Card(
+              elevation: 4,
+              child: ListTile(
+                title: MyText('previous_exams',style: TextStyle(fontFamily: "Cairo",fontSize: 18,fontWeight: FontWeight.w600),),
+                leading: Container(height:double.infinity,child: Icon(Icons.question_answer,color: Colors.blue,size: 18,)),
+                trailing: Icon(Icons.arrow_forward_ios,color: Colors.black,size: 14,),
+              ),
+            ),
+            Card(
+              elevation: 4,
+              child: ListTile(
+                title: MyText('questions_topics',style: TextStyle(fontFamily: "Cairo",fontSize: 18,fontWeight: FontWeight.w600),),
+                leading: Container(height:double.infinity,child: Icon(Icons.polymer,color: Colors.blue,size: 18,)),
+                trailing: Icon(Icons.arrow_forward_ios,color: Colors.black,size: 14,),
+              ),
+            ),
+
+
+          ],
+
+        ),
+        Positioned(
+          bottom: 8,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+               startTestButton(context),
+              SizedBox(height: 8,),
+              questionsAndAnswrsButton(context)
+
+            ],
+          ),
+        )
+
+      ],)
+    );
+  }
+
+  Widget startTestButton(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 8,right: 8),
+      width: MediaQuery.of(context).size.width-16,
+          height: 50,
+          child: RaisedButton(
+            color: ColorProperties.AppColorHex,
+            textColor: Colors.white,
+            onPressed: () {
+            },
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(4),
+            ),
+            child: MyText(
+              "start",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontFamily: "Cairo",
+                  fontWeight: FontWeight.w700),
+            ),
+          ));
+  }
+
+  Widget questionsAndAnswrsButton(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.only(left: 8,right: 8),
+        width: MediaQuery.of(context).size.width-16,
+        height: 50,
+        child: RaisedButton(
+          color: Colors.white,
+          textColor: ColorProperties.AppColor,
+          onPressed: () {
+          },
+          shape: RoundedRectangleBorder(
+            side: BorderSide(color: ColorProperties.AppColor),
+            borderRadius: BorderRadius.circular(4),
+          ),
+          child: MyText(
+            "qustions_answers",
+            style: TextStyle(
+                color: ColorProperties.AppColor,
+                fontSize: 14,
+                fontFamily: "Cairo",
+                fontWeight: FontWeight.w700),
+          ),
+        ));
+  }
+
+}
--- lib/Screens/Home/Home.dart
+++ lib/Screens/Home/Home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tawjihi/Screens/ComonWidget/Text.dart';
+import 'package:tawjihi/Screens/Course/CourseOptionsScreen.dart';
 import 'package:tawjihi/Utils/AppLocalization.dart';
 import 'package:tawjihi/Utils/ColorProperties.dart';
 
@@ -8,71 +9,49 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: ColorProperties.AppColor,
-            title: Text(AppLocalizations.of(context).translate("home"),style: TextStyle(color: Colors.white,fontSize: 20
-            ,fontWeight: FontWeight.w700,fontFamily: "Cairo"),),
-            bottom: tabBar(context),
-            actions: <Widget>[
-              IconButton(
-                icon: Icon(
-                  Icons.settings,
-                  color: Colors.white,
-                ),
-                onPressed: () {
-                },
-              )
-            ],
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: ColorProperties.AppColor,
+          title: Text(
+            AppLocalizations.of(context).translate("home"),
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+                fontFamily: "Cairo"),
           ),
-          body: TabBarView(children: <Widget>[
-            home(context),
-            home(context),
-            home(context)
-          ],),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            backgroundColor: ColorProperties.colorAccent,
-            child: Icon(Icons.phone),
-          ),
+          bottom: tabBar(context),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.settings,
+                color: Colors.white,
+              ),
+              onPressed: () {},
+            )
+          ],
         ),
-
+        body: TabBarView(
+          children: <Widget>[home(context), secondTab(context), thirdTab(context)],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: ColorProperties.colorAccent,
+          child: Icon(Icons.phone),
+        ),
+      ),
     );
   }
 
-  Widget home(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          height: 280,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-            homeTile(context, "اكتشف", Image.asset('assets/images/m.png', fit: BoxFit.fill,)),
-              homeTile(context, "اكتشف", Image.asset('assets/images/m.png', fit: BoxFit.fill,)),
-
-
-            ],),
-        ),
-        Row()
-      ],
-    );
-  
-  }
-
-  Widget header(BuildContext context) {
-  
-  }
-
- Widget  tabBar(BuildContext context) {
+  Widget tabBar(BuildContext context) {
     return TabBar(
       indicatorColor: Colors.blueAccent,
       labelColor: Colors.white,
       isScrollable: true,
       labelPadding: EdgeInsets.only(right: 20, left: 20),
       unselectedLabelColor: Colors.grey,
-      tabs:[
+      tabs: [
         Tab(
           child: Text(AppLocalizations.of(context).translate("main"),
               style: TextStyle(
@@ -85,8 +64,7 @@ class Home extends StatelessWidget {
               style: TextStyle(
                   fontFamily: "Cairo",
                   fontSize: 14.0,
-                  fontWeight: FontWeight.w500)
-          ),
+                  fontWeight: FontWeight.w500)),
         ),
         Tab(
           child: Text(AppLocalizations.of(context).translate("guidance"),
@@ -99,42 +77,154 @@ class Home extends StatelessWidget {
     );
   }
 
+  Widget home(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    final double itemHeight = 220;
+    final double itemWidth = 160;
+    return GridView.count(
+      crossAxisCount: 2,
+      childAspectRatio: itemWidth / itemHeight,
+      children: <Widget>[
+        tile(context, "math", Image.asset(
+              'assets/images/math.png',
+              fit: BoxFit.fill,
+            ),),
+        tile(context, "biology", Image.asset(
+          'assets/images/biology.png',
+          fit: BoxFit.fill,
+        ),),
+        tile(context, "chemistry", Image.asset(
+          'assets/images/chemistry.png',
+          fit: BoxFit.fill,
+        ),),
+        tile(context, "physics", Image.asset(
+          'assets/images/physics.png',
+          fit: BoxFit.fill,
+        ),),
+      ],
+    );
+  }
 
-  Widget homeTile(BuildContext context, String footer, Image image) {
-    return  GestureDetector(
-            child: Stack(
-                      children: <Widget>[
-                        Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8)
-                          ),
-                          child: image,),
-                        Container(
-                          child: Column(
-                            mainAxisAlignment:MainAxisAlignment.center,
-                            children: <Widget>[
-                              Container(
-                                width: 78,
-                                child: Text("الثقافة العامه",style: TextStyle(fontFamily: "Cairo",fontWeight: FontWeight.w700
-                                  ,fontSize: 18,color: Colors.white),),
-                              ),
-                              Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(8),
-                                  color: Colors.white
-                                ),
-                                child: Padding(
-                                  padding: EdgeInsets.symmetric(horizontal: 4,vertical: 0),
-                                    child: Text("اكتشف",style: TextStyle(fontSize: 12,fontWeight: FontWeight.w700
-                                    ,fontFamily: "Cairo"),)),)
+  Widget homeAdby(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    final double itemHeight = 220;
+    final double itemWidth = 160;
+    return GridView.count(
+      crossAxisCount: 2,
+      childAspectRatio: itemWidth / itemHeight,
+      children: <Widget>[
+        tile(context, "math", Image.asset(
+          'assets/images/math.png',
+          fit: BoxFit.fill,
+        ),),
+        tile(context, "sci", Image.asset(
+          'assets/images/scientific_culture.png',
+          fit: BoxFit.fill,
+        ),),
+        tile(context, "history", Image.asset(
+          'assets/images/history.png',
+          fit: BoxFit.fill,
+        ),),
+        tile(context, "geo", Image.asset(
+          'assets/images/geography.png',
+          fit: BoxFit.fill,
+        ),),
+      ],
+    );
+  }
 
+  Widget secondTab(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    final double itemHeight = 220;
+    final double itemWidth = 160;
+    return GridView.count(
+      crossAxisCount: 2,
+      childAspectRatio: itemWidth / itemHeight,
+      children: <Widget>[
+        tile(context, "english", Image.asset(
+          'assets/images/english.png',
+          fit: BoxFit.fill,
+        ),),
+        tile(context, "arabic", Image.asset(
+          'assets/images/arabic.png',
+          fit: BoxFit.fill,
+        ),),
+        tile(context, "islamic", Image.asset(
+          'assets/images/islamic.png',
+          fit: BoxFit.fill,
+        ),),
+        tile(context, "tech", Image.asset(
+          'assets/images/technology.png',
+          fit: BoxFit.fill,
+        ),),
+      ],
+    );
+  }
 
-                            ],
-                          ),
-                        )
-                      ]
-            ),
-    )
-      ;
+  Widget thirdTab(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    final double itemHeight = 220;
+    final double itemWidth = 160;
+    return GridView.count(
+      crossAxisCount: 2,
+      childAspectRatio: itemWidth / itemHeight,
+      children: <Widget>[
+        tile(context, "articles", Image.asset(
+          'assets/images/articles.png',
+          fit: BoxFit.fill,
+        ),),
+        tile(context, "videos", Image.asset(
+          'assets/images/videos.png',
+          fit: BoxFit.fill,
+        ),),
+      ],
+    );
+  }
+
+  Widget tile(BuildContext context, String courseName, Image image) {
+    return GestureDetector(
+      onTap: (){
+        Navigator.of(context).push(MaterialPageRoute(builder: (_)=>CourseList()));
+      },
+      child: Stack(children: <Widget>[
+        Container(
+          height: 400,
+            child: image),
+        Positioned(
+          bottom: 30,
+          right: 20,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                width: 80,
+                margin: EdgeInsets.only(bottom: 4, right: 8, left: 8),
+                child: MyText(courseName,
+                  style: TextStyle(
+                      fontFamily: "Cairo",
+                      fontWeight: FontWeight.w700,
+                      fontSize: 18,
+                      color: Colors.white),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 8),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8), color: Colors.white),
+                child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 0),
+                    child: MyText(
+                      "explore",
+                      style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w700,
+                          fontFamily: "Cairo"),
+                    )),
+              )
+            ],
+          ),
+        )
+      ]),
+    );
   }
 }
